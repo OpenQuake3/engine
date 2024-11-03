cb7f91c5fc8d62e2e7a5593d4370cc9a2cef72ca
sOkam <7308253+heysokam@users.noreply.github.com>
2024-11-03 07:30:03 +0100

    new: `libid3` modules: Cable Connector file

---
+++ src/id3.zig
@@ -0,0 +1,7 @@
+//:______________________________________________________________________
+//  id-Tech3 |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:______________________________________________________________________
+//! @fileoverview Cable connector to all id-Tech3 Modules
+pub usingnamespace @import("./id3/system.zig");
+pub const Cfg = @import("./id3/cfg.zig").Cfg;
+pub usingnamespace @import("./id3/cli.zig");
