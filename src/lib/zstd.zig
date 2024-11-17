eaf3b3d49478054e9cdc929505e16405459cc593
sOkam <7308253+heysokam@users.noreply.github.com>
2024-11-17 05:29:53 +0100

    fmt: Update zstd submodule connector

---
+++ src/lib/zstd.zig
@@ -0,0 +1,5 @@
+//:__________________________________________________________________
+//  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+// @fileoverview Cable connector to the zstd module
+pub usingnamespace @import("./confy/src/lib/zstd.zig");
