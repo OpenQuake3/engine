62b9a4238d9060ed43f61dd0489ed3cea9b3b839
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-09 18:27:43 +0200

    bld: Move `test/dummy` out of butcher

--- build.nim
+++ build.nim
@@ -13,12 +13,16 @@ const engine_version = version(0,0,0)
 #_______________________________________
 # @section Engine: Source Code
 #_____________________________
-const engine_entry  = "entry.c"
+const engine_entry  = "quake3.c"
 const engine_source = @[ # FIX: Get with glob
   "id3/args.c",
   "id3/engine.c",
   "id3/game.c",
   ]
+const engine_flags = Flags(cc: @[
+  "-std=c2x",
+  "-Wno-pre-c23-compat"
+])
 
 #_______________________________________
 # @section QuakeIIIArena: Source Code
@@ -32,16 +36,28 @@ const quake3_source = @[
 # @section QuakeIIIArena: Build Target
 #_____________________________
 const quake3 = Program.new(
-  trg   = engine_name,
-  entry = engine_entry,
-  src   = engine_source & quake3_source,
+  trg     = engine_name,
+  entry   = engine_entry,
+  src     = engine_source & quake3_source,
+  flags   = engine_flags,
+  version = engine_version,
+)
+
+#_______________________________________
+# @section Unit Testing: Build Target
+#_____________________________
+const tests = UnitTest.new(
+  trg   = engine_name&".test",
+  entry = "test.zig",
 )
 
 #_______________________________________
 # @section Build & Run
 #_____________________________
 # Order to build
+tests.build()
 quake3.build()
 # Order to run
+tests.run()
 quake3.run()
 
