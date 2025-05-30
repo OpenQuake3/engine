28d849fc564f414e0e65c9778868adfd2c2d5687
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-30 21:51:21 +0200

    chg: New logger tools that accept varargs
    Requires c23 due to `__VA_OPT__(,)`

--- src/id3/tools/log.h
+++ src/id3/tools/log.h
@@ -5,10 +5,16 @@
 #define H_id3_tools_log
 
 #include "../idtech3/shared.h"
+#include "../cstd.h"
 
 // TODO: echo, print, info, warn, err, debug, etc
-#define id3_log_echo Com_Printf
-#define id3_log_dbg  Com_DPrintf
+#if !defined id3_log_Prefix
+#define id3_log_Prefix "id3"
+#endif
+#define id3_log_echo(fmt, ...) Com_Printf(fmt "\n" __VA_OPT__(,) __VA_ARGS__)
+#define id3_log_info(fmt, ...) Com_Printf("[" id3_log_Prefix ".info] " fmt "\n" __VA_OPT__(,) __VA_ARGS__)
+#define id3_log_warn(fmt, ...) Com_Printf("[" id3_log_Prefix ".warning] " fmt "\n" __VA_OPT__(,) __VA_ARGS__)
+#define id3_log_dbg(fmt, ...) Com_DPrintf("[" id3_log_Prefix ".debug] %s %s %s" fmt "\n", __func__, __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
 
 #endif  // H_id3_tools_log
 
