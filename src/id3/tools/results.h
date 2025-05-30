a3d0e6b1df56086fcdd94440071191db71eeed9d
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-30 21:47:33 +0200

    chg: More `id3_Result` flags for the Minimal Entry Logic

--- src/id3/tools/results.h
+++ src/id3/tools/results.h
@@ -5,8 +5,12 @@
 #define H_id3_tools_results
 
 typedef enum id3_Result {
-  id3_Ok                = 0,
-  id3_ShouldNeverHappen = 123,
+  id3_Ok                = 0,   ///< Generic Success
+  id3_Error             = 1,   ///< Generic Error Code
+  id3_Test42            = 42,  ///< ID for Testing Purposes
+  id3_VersionAndQuit    = 100, ///< Returned when the engine should print the version message and quit immediately
+  id3_HelpAndQuit       = 101, ///< Returned when the engine should print the help message and quit immediately
+  id3_ShouldNeverHappen = 123, ///< Error code printed by the engine when the code reaches the end of the main function.
   id3_Result_ForceU8    = 255,
 } id3_Result;
 
