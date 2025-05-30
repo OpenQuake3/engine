a3c9d35e6f52a45e7992c706c8f7256c0aa66252
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-30 21:52:52 +0200

    new: Cross-platform CLI Initialization (sketch)

--- src/id3/system/cli.c
+++ src/id3/system/cli.c
@@ -1,8 +1,68 @@
+//:__________________________________________________________________
+//  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
 #include "./cli.h"
 
-id3_CLI id3_cli_init (
+
+id3_Result id3_cli_shouldVersionHelpAndQuit (
   id3_Args const* const cli
 ) {
-  return (id3_CLI){ .args = *cli };
+  for (Sz id = 0; id < cli->argc; ++id) {
+    cstr const arg = cli->argv[id];
+    if (std_cstr_equal(arg, "--version")) return id3_VersionAndQuit;
+    if (std_cstr_equal(arg, "--help")) return id3_HelpAndQuit;
+  }
+  return id3_Ok;
+}
+
+
+void id3_cli_maybeVersionHelpAndQuit (
+  id3_Args const* const cli
+) {
+  switch (id3_cli_shouldVersionHelpAndQuit(cli)) {
+    case id3_HelpAndQuit : {
+      id3_log_info("TODO: Help Message Here");
+      std_exit(id3_HelpAndQuit);
+    }
+    case id3_VersionAndQuit : {
+      id3_log_info("TODO: Version Message Here");
+      std_exit(id3_VersionAndQuit);
+    }
+    default : (void)cli; /* discard */
+  }
+}
+
+
+void id3_cli_parse_early (
+  id3_Args const* const args
+) {
+  char con_title[id3_cvar_value_string_MaxLen];
+  int  xpos = 0;
+  int  ypos = 0;
+  /*useXYpos = */ Com_EarlyParseCmdLine(args->merged, con_title, sizeof(con_title), &xpos, &ypos);
+}
+
+
+id3_CLI id3_cli_create (
+  id3_Args const* const args
+) {
+  return (id3_CLI){
+    .args = id3_args_create((int)args->argc, args->argv),
+  };
+}
+
+
+void id3_cli_init (
+  id3_CLI* cli
+) {
+  id3_cli_maybeVersionHelpAndQuit(&cli->args);
+  //__________________________________________________
+  // TODO: mac
+  // #ifdef __APPLE__
+  // Sys_SetBinaryPath( argv[ 0 ] );
+  // Sys_SetDefaultBasePath( Sys_StripAppBundle( binaryPath ) );
+  // #endif
+  //__________________________________________________
+  id3_cli_parse_early(&cli->args);
 }
 
--- src/id3/system/cli.h
+++ src/id3/system/cli.h
@@ -4,13 +4,39 @@
 #if !defined H_id3_system_cli
 #define H_id3_system_cli
 
+// @deps idtech3
+#include "../idtech3/shared.h"
+#include "../idtech3/tools/cli.h"
+#include "../idtech3/config/cvar.h"
+// @deps engine
+#include "../tools/results.h"
 #include "../tools/args.h"
+#include "../tools/log.h"
 
 typedef struct id3_CLI {
   id3_Args args;
 } id3_CLI;
 
-id3_CLI id3_cli_init (id3_Args const* const cli);
+/// @description
+/// Returns a base id3_CLI object without parsing the arguments.
+id3_CLI id3_cli_create (id3_Args const* const args);
+
+/// @description
+/// Returns whether or not the arguments given request to print a help/version message and quit
+id3_Result id3_cli_shouldVersionHelpAndQuit (id3_Args const* const args);
+
+/// @description
+/// Simple parse the given cli arguments and quit when they request to print a help/version message
+/// @reference Cross-platform rewrite of `unix_main.c/Sys_ParseArgs`, which only managed `--version` and `--help` arguments
+std_pragma_MayNotReturn void id3_cli_maybeVersionHelpAndQuit (id3_Args const* const args);
+
+/// @description
+/// Alias for `Com_EarlyParseCmdLine` that ignores the outdated parts of the function.
+void id3_cli_parse_early (id3_Args const* const args);
+
+/// @description
+/// Parse the arguments of the given cli object to create the internal data used by the engine
+std_pragma_MayNotReturn void id3_cli_init (id3_CLI* cli);
 
 #endif  // H_id3_system_cli
 
