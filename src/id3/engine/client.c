1408c2e1faae2c30aacaf72917c59640c4bc8dc0
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-30 21:54:47 +0200

    new: Client Initialization Entry Points

--- src/id3/engine/client.c
+++ src/id3/engine/client.c
@@ -3,11 +3,21 @@
 //:__________________________________________________________________
 #include "./client.h"
 
+
 id3_Client id3_cl_init (
-  id3_Args const* const cli
+  id3_CLI const* const cli,
+  id3_Common const* const co
 ) {
+  std_discard(co);
   return (id3_Client){
     .system = id3_sys_init(cli),
   };
+}
+
+
+void id3_cl_term (
+  id3_Client* cl
+) {
+  id3_sys_term(&cl->system);
 }
 
--- src/id3/engine/client.h
+++ src/id3/engine/client.h
@@ -1,8 +1,32 @@
+//:__________________________________________________________________
+//  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+#if !defined H_id3_engine_client
+#define H_id3_engine_client
+
+#include "../idtech3/engine/client.h"
+#include "../cstd.h"
+#include "../tools/results.h"
+#include "../tools/log.h"
 #include "../system.h"
+#include "./common.h"
 
 typedef struct id3_Client {
   id3_System system;
 } id3_Client;
 
-id3_Client id3_cl_init (id3_Args const* const cli);
+
+/// @description
+/// Initializes/allocates all the data used by the Client Engine
+id3_Client id3_cl_init (id3_CLI const* const cli, id3_Common const* const co);
+
+/// @description
+/// Terminates/deallocates all the data used by the given Client Engine
+void id3_cl_term (id3_Client* cl);
+
+/// @description
+/// Returns true when the client is not allowed to slowdown gameplay for this frame (eg: When Recording)
+#define id3_cl_noDelay CL_NoDelay
+
+#endif  //H_id3_engine_client
 
