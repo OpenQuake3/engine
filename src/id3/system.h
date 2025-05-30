1451d0c3819e52621263755afe76e968ccf474ec
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-30 21:48:17 +0200

    chg: Better `id3/system` init/term functions

--- src/id3/system.h
+++ src/id3/system.h
@@ -7,14 +7,23 @@
 #include "./system/cli.h"
 #include "./system/input.h"
 #include "./system/window.h"
+#include "./system/fpu.h"
 
 typedef struct id3_System {
   id3_CLI    cli;
   id3_Input  input;
-  id3_Window window;  // TODO:
+  id3_Window window;  //< TODO: GLFW
 } id3_System;
 
-id3_System id3_sys_init (id3_Args const* const cli);
+/// @description
+/// Initializes/allocates all system-specific data used by the engine.
+/// ie. Input, Window, CLI args, etc
+id3_System id3_sys_init (id3_CLI const* const cli);
+
+/// @description
+/// Terminates/deallocates all system-specific data used by the engine.
+/// ie. Input, Window, CLI args, etc
+void id3_sys_term (id3_System* sys);
 
 #endif  // H_id3_system
 
--- src/id3/system/core.c
+++ src/id3/system/core.c
@@ -4,12 +4,20 @@
 #include "../system.h"
 
 id3_System id3_sys_init (
-  id3_Args const* const cli
+  id3_CLI const* const cli
 ) {
   return (id3_System){
-    .cli    = id3_cli_init(cli),
+    .cli    = *cli,
     .input  = id3_sys_input_init(),
     .window = id3_sys_window_init(),
   };
+}
+
+void id3_sys_term (
+  id3_System* sys
+) {
+  std_discard(sys->cli);
+  std_discard(sys->input);
+  std_discard(sys->window);
 }
 
