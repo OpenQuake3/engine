fa7cd1646d761298ee8d9cabb3a5f4c145f49ffe
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-18 11:22:09 +0200

    new: Engine.Event tools sketch for `Com_Init`

---
+++ src/id3/engine/event.h
@@ -0,0 +1,14 @@
+//:__________________________________________________________________
+//  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+#ifndef H_id3_engine_event
+#define H_id3_engine_event
+// @deps idtech3
+#include "../idtech3/engine/event.h"
+
+/// @internal Dont use. Only used to take ownership of Com_Init
+/// @descr Initializes the PushEvent internal GlobalState.
+#define id3_event_push_init Com_InitPushEvent
+
+#endif  // H_id3_engine_event
+
