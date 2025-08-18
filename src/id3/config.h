75db18e0a51ccfb70ae31b3f4ef200590d0770fc
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-18 11:18:18 +0200

    fmt: Move `id3.meta` config options to their own file

--- src/id3/config.h
+++ src/id3/config.h
@@ -1,28 +1,13 @@
 //:__________________________________________________________________
 //  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:__________________________________________________________________
-#if !defined H_id3_config
+#ifndef H_id3_config
 #define H_id3_config
 
+#include "./config/meta.h"
 #include "./config/cvar.h"
 #include "./config/engine.h"
 #include "./config/game.h"
-
-#if !defined id3_meta_BuildDate
-#undef id3_meta_BuildDate
-#define id3_meta_BuildDate "UnknownBuildDate"
-#endif  // id3_meta_BuildDate
-
-#if !defined id3_meta_BuildPlatform
-#undef id3_meta_BuildPlatform
-#define id3_meta_BuildPlatform "UnknownBuildPlatform"
-#endif  // id3_meta_BuildPlatform
-
-
-#if !defined id3_meta_Version
-#undef id3_meta_Version
-#define id3_meta_Version "UnknownVersion"
-#endif  // id3_meta_Version
 
 #endif  // H_id3_config
 
---
+++ src/id3/config/meta.h
@@ -0,0 +1,29 @@
+//:__________________________________________________________________
+//  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+#ifndef H_id3_config_meta
+#define H_id3_config_meta
+
+#if !defined id3_meta_BuildDate
+#undef id3_meta_BuildDate
+#define id3_meta_BuildDate "UnknownBuildDate"
+#endif  // id3_meta_BuildDate
+
+#if !defined id3_meta_BuildPlatform
+#undef id3_meta_BuildPlatform
+#define id3_meta_BuildPlatform "UnknownBuildPlatform"
+#endif  // id3_meta_BuildPlatform
+
+
+#if !defined id3_meta_Version
+#undef id3_meta_Version
+#define id3_meta_Version "UnknownVersion"
+#endif  // id3_meta_Version
+
+#if !defined id3_meta_Name
+#undef id3_meta_Name
+#define id3_meta_Name "UnknownEngine"
+#endif  // id3_meta_Name
+
+#endif  // H_id3_config_meta
+
