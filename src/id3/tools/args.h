bc302fd3344d3a2ff2366e5033bfd5569d1ebec1
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-18 11:16:04 +0200

    fix: Update `id3.Args` module for the `pointer` and `cstring` types

--- src/id3/tools/args.h
+++ src/id3/tools/args.h
@@ -1,19 +1,19 @@
 //:__________________________________________________________________
 //  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:__________________________________________________________________
-#if !defined H_id3_tools_args
+#ifndef H_id3_tools_args
 #define H_id3_tools_args
 
 #include "../cstd.h"
 #include "../idtech3/config/cvar.h"
 
 typedef struct id3_Args {
-  Sz        argc;
-  cstr_list argv;
-  char*     merged;
+  Sz               argc;
+  std_cstring_list argv;
+  char*            merged;
 } id3_Args;
 
-id3_Args id3_args_create (int const argc, cstr_list const argv);
+id3_Args id3_args_create (int const argc, std_cstring_list const argv);
 
 #endif  // H_id3_tools_args
 
